0859301cd4c66464733537737f79f35fd304c91a
J <j@Js-MacBook-Pro.local>
2019-06-07 22:09:28 -0400

    removed boilerplate func

--- handler.js
+++ handler.js
@@ -4,23 +4,6 @@ const moment = require('moment-timezone');
 
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  let body;
-  if (event.body) {
-    body = JSON.parse(event.body);
-  }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
 module.exports.weekRange = async (event, context) => {
   const currDate = moment();
   const endOfWeekDate = moment().add(4, 'days');
